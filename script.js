032ca0f2ce2d27d526ab0af846c118167c2f8916
adam <adam.sobiesak@gmail.com>
2019-11-19 23:18:34 +0100

    delete button and add some js

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
-const figuresColor = ["red", "green", "blue", "yellow"];
+const figuresColor = ["cyan", "magenta", "black", "yellow"];
+const bgColor = ["cyan", "magenta", "black", "yellow"];
 
-let figures = document.querySelectorAll("div.figure");
+let figures = document.querySelectorAll("section>div");
+let header = document.querySelectorAll("header>div");
+
 figures = [...figures]; // pobrana lista query na tablice
 
+const startTime = new Date().getTime();
+const positionbg = Math.floor(Math.random()*bgColor.length);
+
+let activeColor = ''; // aktulanie aktywny kolor
+const clickedColor = [bgColor[positionbg]];
+
+const colorMatch = 1;
+let colorResult = 0;
+
+
+const clickColor = function(){
+    activeColor = this;
+   
+    clickedColor[1] = activeColor;
+        console.log(clickedColor);
+    if(clickedColor[0].className === clickedColor[1].className) {
+            console.log('wygrana');
+        };
+            
+    
+};
+    
 const init = () => {
-    figures.forEach(function(figure){
+    header.forEach(head => {
+        head.classList.add(clickedColor);})
+    figures.forEach(figure => {
         const position = Math.floor(Math.random()*figuresColor.length);
-        figure.classList.add(figuresColor[position]);
+        figure.classList.add(figuresColor[position]); // dodawanie wylosowanej klasy, jako index wylosowana wartość
+        figuresColor.splice(position, 1);
     })
+  
+}
+const klik = () => {
+    figures.forEach(figure => {
+    figure.addEventListener('click', clickColor)})
+    
 }
 
+
 init()
+klik()
